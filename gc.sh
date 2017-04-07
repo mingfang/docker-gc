e3c84565092eddf2577b01dbb6162da87b16f2dd
root <root@minux.home>
2017-04-07 13:59:32 -0400

    FORCE_CONTAINER_REMOVAL

--- gc.sh
+++ gc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-FORCE_IMAGE_REMOVAL=1 /docker-gc/docker-gc
+FORCE_CONTAINER_REMOVAL=1 FORCE_IMAGE_REMOVAL=1 /docker-gc/docker-gc
 docker images --filter dangling=true -q|xargs --no-run-if-empty docker rmi
